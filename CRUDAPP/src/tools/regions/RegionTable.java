3d9e53b7dfe7614fe6605a6f2a12add88ba84f50
haikalmadani <muhammadhaikalmadani@gmail.com>
2021-07-15 15:15:44 +0700

    Update RegionTable.java

--- CRUDAPP/src/tools/regions/RegionTable.java
+++ CRUDAPP/src/tools/regions/RegionTable.java
@@ -15,24 +15,27 @@ import tools.DB_Connections;
  * @author haikal
  */
 public class RegionTable {
-    public static void insertRegion() {        
-    DB_Connections connection = new DB_Connections();
-    RegionDAO rdao = new RegionDAO(connection.getConnection());
     
-    Scanner insReg = new Scanner(System.in);
-    String nama_region, id_region;
-    int id_region2;
+    public static DB_Connections connection = new DB_Connections();
+    public static RegionDAO rdao = new RegionDAO(connection.getConnection());
+    
+    public static Scanner inpReg = new Scanner(System.in);
+    public static String nama_region, id_region;
+    public static int id_region2;
+    
+    public static void insertRegion() {        
+    
+    Region inReg = new Region();
     System.out.println("Masukkan ID Region = ");
-    id_region = insReg.nextLine();
+    id_region = inpReg.nextLine();
     id_region2 = Integer.parseInt(id_region);                
+    inReg.setIdReg(id_region2);
     System.out.println("Masukkan Nama Region = ");
-    nama_region = insReg.nextLine();
+    nama_region = inpReg.nextLine();
+    inReg.setNameReg(nama_region);
     System.out.println("\nID Region :"+nama_region);
     System.out.println("\nRegion :"+id_region);
         
-    Region inReg = new Region();
-    inReg.setIdReg(id_region2);
-    inReg.setNameReg(nama_region);
         
     if (rdao.insert(inReg)) {
        System.out.println("BERHASIL INSERT");
@@ -40,19 +43,14 @@ public class RegionTable {
     
 }
     public static void updateRegion() {
-    DB_Connections connection = new DB_Connections();
-    RegionDAO rdao = new RegionDAO(connection.getConnection());
 
     Region reg = new Region();
-    Scanner updReg = new Scanner(System.in);
-    String nama_region, id_region;
-    int id_region2;
     System.out.println("Masukkan ID Region = ");
-    id_region = updReg.nextLine();
+    id_region = inpReg.nextLine();
     id_region2 = Integer.parseInt(id_region);                
     reg.setIdReg(id_region2);              
     System.out.println("Masukkan Nama Region = ");
-    nama_region = updReg.nextLine();
+    nama_region = inpReg.nextLine();
     reg.setNameReg(nama_region);
     System.out.println("ID Region :"+id_region2);
     System.out.println("Region :"+nama_region);
@@ -64,26 +62,20 @@ public class RegionTable {
 }
     
     public static void deleteRegion() {
-    DB_Connections connection = new DB_Connections();
-    RegionDAO rdao = new RegionDAO(connection.getConnection());
-
-    Scanner deltReg = new Scanner(System.in);
-    int id_region2;
+    
+    Region delReg = new Region();
     System.out.println("Masukkan ID Region = ");
-    id_region2 = deltReg.nextInt();
+    id_region2 = inpReg.nextInt();
+    delReg.setIdReg(id_region2);
     System.out.println("\nID Region :"+id_region2);
 
-    Region delReg = new Region();
-    delReg.setIdReg(id_region2);
     if (rdao.delete(delReg)) {
         System.out.println("DELETE SUKSES");
     }
 }
     
     public static void showRegion() {
-    DB_Connections connection = new DB_Connections();
-    RegionDAO rdao = new RegionDAO(connection.getConnection());
-
+   
     for (Region r : rdao.getAll()) {
         System.out.println(r.getIdReg()+" - "+r.getNameReg());
     }      
